9a5fc9564e5bca24fdc662f2305da5892c028b86
Sebastian Kuśnierz <sebastiano139@gmail.com>
2022-07-12 17:01:31 +0200

    Remove unused code

--- template/.storybook/preview.js
+++ template/.storybook/preview.js
@@ -7,19 +7,3 @@ import StoreDecorator from './storeDecorator';
 addDecorator(ThemeDecorator);
 addDecorator(RouterDecorator);
 addDecorator(StoreDecorator);
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  customizeAntdTheme: {
-    modifyVars: {
-      white: '#ff1771',
-      'border-radius-base': '20px',
-    },
-  },
-};
